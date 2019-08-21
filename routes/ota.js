64a6c0aa3c857bf76bdecb069f0cdcfdb32b5386
john0504 <john2000@mail2000.com.tw>
2019-08-21 13:25:27 +0800

    test ota download

--- routes/ota.js
+++ routes/ota.js
@@ -27,4 +27,20 @@ router.get('/az', function (req, res) {
     return;
 });
 
+router.get('/azz', function (req, res) {
+    var mysqlQuery = req.mysqlQuery;
+    var sha1 = req.query["sha1"];
+
+    mysqlQuery("SELECT * FROM FirmwareTbl WHERE sha1 = ?", sha1, function (err, firmware) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+        if (firmware && firmware.length > 0) {
+            var fs = require('fs');
+            res.status(200).send(fs.readFileSync(`./${firmware[0].FilePath}`));
+        }
+    });
+});
+
 module.exports = router;
