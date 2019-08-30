0675f7187cf33ffd982f5321ffb15f49e5311ccf
john0504 <john2000@mail2000.com.tw>
2019-08-30 12:35:25 +0800

    update reboot cmd

--- app.js
+++ app.js
@@ -359,7 +359,7 @@ client.on('message', function (topic, msg) {
 
                                     var DevNo = result[0].DevNo
                                     var mytopic = `${PrjName}/${DevNo}/D`
-                                    var mymsg = { Account: "0000", CMD: "REBOOT" };
+                                    var mymsg = { Account: "0000", CMD: "REBOOT", Owner: No };
                                     client.publish(mytopic, JSON.stringify(mymsg), { qos: 1, retain: true });
                                     return;
                                 });
