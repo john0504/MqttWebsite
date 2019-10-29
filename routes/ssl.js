f5a451d0c7c85e6d8beeac6b7013cdab19f58452
john0504 <john2000@mail2000.com.tw>
2019-10-29 14:29:56 +0800

    update ssl

--- routes/ssl.js
+++ routes/ssl.js
@@ -1,9 +1,9 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/sCFbhAUFN_BUU0yOz83hc4CDDoXs0vzTOV7Qc5dt5SQ', function (req, res) {
+router.get('/WoutOVDroQgv22qEjzx-zU8vhGHYmjHf1ddJiofHoKM', function (req, res) {
     var fs = require('fs');
-    var file = fs.readFileSync(`./sCFbhAUFN_BUU0yOz83hc4CDDoXs0vzTOV7Qc5dt5SQ`);
+    var file = fs.readFileSync(`./WoutOVDroQgv22qEjzx-zU8vhGHYmjHf1ddJiofHoKM`);
     res.status(200).send(file.toString());
     return;
 });
