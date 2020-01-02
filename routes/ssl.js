bf40b4f2163cb7b595d4e68502a817878692602a
john0504 <john2000@mail2000.com.tw>
2020-01-02 09:22:20 +0800

    update ssl

--- routes/ssl.js
+++ routes/ssl.js
@@ -1,9 +1,9 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/WoutOVDroQgv22qEjzx-zU8vhGHYmjHf1ddJiofHoKM', function (req, res) {
+router.get('/UuqvsqaBKnm86e7B82m8yBjNYaXlqNpGvHeo9LKRsz4', function (req, res) {
     var fs = require('fs');
-    var file = fs.readFileSync(`./WoutOVDroQgv22qEjzx-zU8vhGHYmjHf1ddJiofHoKM`);
+    var file = fs.readFileSync(`./UuqvsqaBKnm86e7B82m8yBjNYaXlqNpGvHeo9LKRsz4`);
     res.status(200).send(file.toString());
     return;
 });
