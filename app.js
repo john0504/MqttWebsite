ad999583e89ce38be2bb9a1cd03243232039b9ed
john0504 <john2000@mail2000.com.tw>
2020-03-09 09:07:44 +0800

    update app version

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ client.on('connect', function () {
     console.log('MQTT server connected.');
     client.subscribe("WAWA/#");
     var topic = `WAWA2/AppVersion`;
-    var paylod = `2.4.20200114`;
+    var paylod = `2.4.20200306`;
     client.publish(topic, paylod, { qos: 1, retain: true });
 });
 
