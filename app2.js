a7c9666933d22d7432035b7aaa248611ab0ad0ea
JohnWu <john.wu@aurorailc.com>
2020-06-18 16:51:39 +0800

    update ssl

--- app2.js
+++ app2.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-//app.use('/.well-known/acme-challenge', ssl);
-app.use('/.well-known/pki-validation', ssl);
+app.use('/.well-known/acme-challenge', ssl);
+// app.use('/.well-known/pki-validation', ssl);
 
 
 // catch 404 and forward to error handler
