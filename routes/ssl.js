1511e9d3d30706c5620443f2d4ee8cb7ee85e405
JohnWu <john.wu@aurorailc.com>
2020-06-19 11:12:08 +0800

    update ssl

--- routes/ssl.js
+++ routes/ssl.js
@@ -1,9 +1,9 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/7CF32140CC7EB30A27687FAA2A9FD497.txt', function (req, res) {
+router.get('/32C017A8FF7A9BB97E4499DFB268FD7E.txt', function (req, res) {
     var fs = require('fs');
-    var file = fs.readFileSync(`./7CF32140CC7EB30A27687FAA2A9FD497.txt`);
+    var file = fs.readFileSync(`./32C017A8FF7A9BB97E4499DFB268FD7E.txt`);
     res.status(200).send(file.toString());
     return;
 });
